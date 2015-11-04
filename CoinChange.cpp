ec3d7a9e21129d43d52b3ca581b9778a954a4ec6
Fiona Ding <fding253@gmail.com>
2015-11-03 23:16:18 -0800

    Edited Coin Change problem

--- CoinChange.cpp
+++ CoinChange.cpp
@@ -2,51 +2,68 @@
 #include <vector>
 #include <utility>
 #include <ctime>
+#include <algorithm>
 
 using namespace std;
 
-int waysToMakeChange(int coins, int total) {
-    vector< vector <int> > solution(coins+1, vector<int>(total+1));
+int waysToMakeChange(vector<int>& coins, int total) {
+    if (find(coins.begin(), coins.end(), 0) == coins.end()) {
+        // coins does not contain 0: add it
+        coins.push_back(0);
+    }
     
-//    if(coins > total) return waysToMakeChange(total, total);
+    vector< vector <long int> > solution(coins.size(), vector<long int>(total+1));
+    
+    sort(coins.begin(), coins.end());
     
     // Base Cases
-    for (int i=0; i<=coins; i++) {
-        solution[i][0] = 1; //1 way to make change for amount 0
+    for (int i=0; i<coins.size(); i++) {
+        solution[i][0] = (long int)1; //1 way to make change for amount 0
     }
     for (int j=1; j<=total; j++) {
-        solution[0][j] = 0; // no ways to make non-zero amount out of 0 coins
+        solution[0][j] = (long int)0; // no ways to make non-zero amount out of 0 coins
     }
     
     
-    for (int i=1; i<=coins; i++) {
+    for (int i=1; i<coins.size(); i++) {
         for (int j=1; j<=total; j++) {
-            if (i>j) {
+            if (coins[i]>j) {
                 solution[i][j] = solution[i-1][j];
             }
             else {
-                solution[i][j] = solution[i-1][j] + solution[i][j-i];
+                solution[i][j] = solution[i-1][j] + solution[i][j-coins[i]];
             }
         }
     }
     
-    return solution[coins][total];
+    return solution[coins.size()-1][total];
 }
 
-int main() {
-/*    cout << "Coins = 0, Amount = 5: " << waysToMakeChange(0,5) << endl;
-    cout << "Coins = 3, Amount = 5: " << waysToMakeChange(3,5) << endl;
-    cout << "Coins = 3, Amount = 7: " << waysToMakeChange(3,7) << endl;
-*/
-
-    clock_t begin = clock();
+void printTable(vector<int>& coins, int total) {
+    if (find(coins.begin(), coins.end(), 0) == coins.end()) {
+        // coins does not contain 0: add it
+        coins.push_back(0);
+    }
     
-    for (int i=0; i<11; i++) {
-        for (int j=0; j<11; j++) {
-            cout << waysToMakeChange(i,j) << "\t";
+    vector<int> subset;
+  
+    for (int i=0; i<coins.size(); i++) {
+        subset.push_back(coins[i]);
+        for (int j=0; j<total; j++) {
+            cout << waysToMakeChange(subset, j) << "\t";
         }
         cout << endl;
     }
+    
+}
+
+int main() {
+
+    clock_t begin = clock();
+    
+    vector<int> coins {0,1,2,3,4,5};
+
+    printTable(coins, 11);
     
     clock_t stop = clock();
     double elapsed = (double) (stop-begin) / CLOCKS_PER_SEC;
